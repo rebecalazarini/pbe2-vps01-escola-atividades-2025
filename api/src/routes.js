149c69c75cc18d58d2747a06ec575c08b2df2c02
rebecalazarini <rebecalazarini2007@gmail.com>
2025-04-15 14:22:45 -0300

    alterações

--- api/src/routes.js
+++ api/src/routes.js
@@ -2,12 +2,32 @@ const express = require('express');
 const routes = express.Router();
 
 const Aluno = require('./controllers/aluno.js');
-const Aluno = require('./controllers/telefone.js');
-const Aluno = require('./controllers/atividade.js');
+const Telefone = require('./controllers/telefone.js');
+const Atividade = require('./controllers/atividade.js');
 
 
 routes.get('/', (req, res) => {
   return res.json({ titulo: 'Escola ACME' });
 });
+
+
+router.post('/a',Aluno.create);
+router.get('/a',Aluno.read);
+router.get('/a/:id',Aluno.readOne);
+router.patch('/a/:id',Aluno.update);
+router.delete('/a/:id',Aluno.remove);
+
+router.post('/t',Telefone.create);
+router.get('/t',Telefone.read);
+router.get('/t/:id',Telefone.readOne);
+router.patch('/t/:id',Telefone.update);
+router.delete('/t/:id',Telefone.remove);
+
+router.post('/at',Atividade.create);
+router.get('/at',Atividade.read);
+router.get('/at/:id',Atividade.readOne);
+router.patch('/at/:id',Atividade.update);
+router.delete('/at/:id',Atividade.remove);
+
 
 module.exports = routes;
